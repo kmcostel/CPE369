5dcee91c144daca11779d59e4e50b211df5cd3db
Holly Haraguchi <holly.haraguchi@yahoo.com>
2017-01-13 17:05:26 -0800

    Added error checking to generator driver and updated README

--- lab1/part1/thghtShreGen.java
+++ lab1/part1/thghtShreGen.java
@@ -1,29 +1,41 @@
+/* Authors: Holly Haraguchi (hharaguc@calpoly.edu), Kevin Costello (kmcostel@calpoly.edu) */
+
 import java.io.*;
 import org.json.simple.*;
 
 /* Data generation program */
 public class thghtShreGen {
    
+   /* Assumes that valid CML arguments will be given in the following order:
+    * |numObjs| |outputFileName|
+    */
    public static void main (String args[]) throws FileNotFoundException, IOException {
-      /* Assume perfect user input for now */
-
+      /* Valid user input */
       if (args.length == 2) {
          String outFile = args[0];
-         int numObjs = Integer.parseInt(args[1]);
+         try {
+            int numObjs = Integer.parseInt(args[1]);
+            JsonGen gen = new JsonGen(numObjs, outFile);
+         }
+         catch (Exception e) {
+            System.out.println("ERROR: " + e.toString());
+            System.out.println("Usage: thghtShreGen <numJsonObjects> <outputFileName>")
+         }      
          
-         JsonGen gen = new JsonGen(numObjs, outFile);
          try (FileWriter file = new FileWriter(outFile)) {
             for (int i = 0; i < numObjs; i++) {
                file.write(gen.JSON_Object().toJSONString() + "\n");
             } 
             file.close();
          }
+         catch (Exception e) {
+            System.out.println("Error opening '" + outFile + "': " + e.toString());
+            System.out.println("Usage: thghtShreGen <numJsonObjects> <outputFileName>")
+         }
+      }      
+      else {
+         System.out.println("Usage: thghtShreGen <numJsonObjects> <outputFileName>")
       }
-      
-      /* possible option param (as mentioned in the spec): word file for messages */
-      
-
-      
    }
 
 }
