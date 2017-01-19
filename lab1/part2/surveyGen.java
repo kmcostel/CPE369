4a9b0ad46fc01cfbeb518624fbaf482dd5ac45ef
kmcostel <kevincostello213@gmail.com>
2017-01-18 18:05:00 -0800

    Pulling

---
+++ lab1/part2/surveyGen.java
@@ -0,0 +1,48 @@
+/* 
+ * Lab 1-2
+ * Authors: Holly Haraguchi (hharaguc@calpoly.edu) and Kevin Costello (kmcostel@calpoly.edu)
+ * CPE 369, Winter 2016
+ */
+import java.io.*;
+import org.json.simple.*;
+
+/* Data generation program */
+public class surveyGen {
+   
+   /* Assumes that valid CML arguments will be given in the following order:
+    * |outputFileName| |numObjs| 
+    */
+   public static void main (String args[]) throws FileNotFoundException, IOException {
+      int numObjs = -1;
+
+      /* Valid user input */
+      if (args.length == 2) {
+         String outFile = args[0];
+         try {
+            numObjs = Integer.parseInt(args[1]);
+         }
+         catch (Exception e) {
+            System.out.println("ERROR: " + e.toString());
+            System.out.println("Usage: surveyGen <outputFileName> <numJsonObjects>");
+         }      
+         
+         /* Initialize the SurveyJson object */
+         SurveyJson gen = new SurveyJson();
+
+         try (FileWriter file = new FileWriter(outFile)) {
+            for (int i = 0; i < numObjs; i++) {
+               file.write(gen.genObject().toJSONString() + "\n");
+            } 
+            file.close();
+         }
+         catch (Exception e) {
+            System.out.println("Error opening '" + outFile + "': " + e.toString());
+            System.out.println("Usage: surveyGen <outputFileName> <numJsonObjects>");
+         }
+      }      
+      else {
+         System.out.println("Usage: surveyGen <outputFileName> <numJsonObjects>");
+      }
+   }
+
+}
