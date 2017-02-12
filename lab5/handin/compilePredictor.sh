96b309a79ddc3161415fd1d37355b3b7fc0f89d1
Holly <hharaguc@calpoly.edu>
2017-02-12 13:53:01 -0800

    Finished--handin .zip and README

--- lab5/handin/compilePredictor.sh
+++
@@ -1,6 +0,0 @@
-# Holly Haraguchi and Kevin Costello
-# Lab 5
-# CPE 369, Winter 2017
-# Compiles ratingsPredictor.java with the necessary .jar files
-
-javac -cp json-simple-1.1.1.jar:mongo-java-driver-3.4.2.jar ratingsPredictor.java
