0acfd3b98dd6bc452805f5b7c6aa6229b848c102
Holly <hharaguc@calpoly.edu>
2017-02-11 17:39:49 -0800

    Updated bash scripts with comments + 1 CML arg; adding JSON input file

--- lab5/compilePredictor.sh
+++ lab5/compilePredictor.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+# Holly Haraguchi and Kevin Costello
+# Lab 5
+# CPE 369, Winter 2017
+# Compiles ratingsPredictor.java with the necessary .jar files
 
 javac -cp json-simple-1.1.1.jar:mongo-java-driver-3.4.2.jar ratingsPredictor.java
--- lab5/runPredictor.sh
+++ lab5/runPredictor.sh
@@ -1,1 +1,7 @@
-java -cp json-simple-1.1.1.jar:mongo-java-driver-3.4.2.jar:. ratingsPredictor
+# Holly Haraguchi and Kevin Costello
+# Lab 5
+# CPE 369, Winter 2017
+# Runs ratingsPredictor and uses the first CML argument as the program's JSON input file
+
+name=$1
+java -cp json-simple-1.1.1.jar:mongo-java-driver-3.4.2.jar:. ratingsPredictor $name 
