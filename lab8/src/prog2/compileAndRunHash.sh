6471ff628a3fcb5342b4b8273db7eed4ee7b82ea
kmcostel <kmcostel@calpoly.edu>
2017-03-01 13:26:07 -0800

    Cleaned up compile and run script

--- lab8/src/prog2/compileAndRunHash.sh
+++ lab8/src/prog2/compileAndRunHash.sh
@@ -1,10 +1,8 @@
 # Kevin Costello, Holly Haraguchi
 #!/bin/bash
 
-# This seems like it is definitely correct
 hadoop com.sun.tools.javac.Main hashtags.java
 
 jar cvf hashtags.jar hashtags*class
 
 hadoop jar hashtags.jar hashtags -libjars json-simple-1.1.1.jar lab8_inputs/hashtags-input.json lab8_outputs/prog2
-
