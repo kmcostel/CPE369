14201de9b12280fbd1ed09597377872023f0a3ba
kmcostel <kmcostel@calpoly.edu>
2017-03-03 15:48:32 -0800

    Not running

--- lab8/src/prog3/compileAndRunFave.sh
+++ lab8/src/prog3/compileAndRunFave.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 hadoop com.sun.tools.javac.Main favoriteMovie.java
+
 jar cvf favoriteMovie.jar favoriteMovie*class
-hadoop jar favoriteMovie.jar favoriteMovie -libjars ../json-simple-1.1.1.jar favoriteMovie-input.json favoriteMovie-output
+
+hadoop jar favoriteMovie.jar favoriteMovie -libjars ../json-simple-1.1.1.jar favoriteMovie-input.json movies.csv prog3-out
 
--- lab8/src/prog3/favoriteMovie.java
+++ lab8/src/prog3/favoriteMovie.java
@@ -11,7 +11,10 @@ import org.apache.hadoop.mapreduce.Job; // the MapReduce job class that is used 
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat; // class for "pointing" at input file(s)
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat; // class for "pointing" at output file
 import org.apache.hadoop.fs.Path;                // Hadoop's implementation of directory path/filename
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs; // class for pointing as multiple input files
+
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat; 
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.conf.Configuration; // Hadoop's configuration object
 import org.apache.hadoop.conf.Configured;
 
@@ -23,6 +26,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 
 // Exception handling
 import java.io.IOException;
@@ -36,9 +40,9 @@ import java.io.IOException;
  * Reducer1 will figure out the Movie's title by splitting the value on ",". The value with
  *  the movie's title will return a string[] of length=1, while all other values will have
  *  a length=2 (State, Rating).
- * Reducer1 will emit <State, MovieTitle> as Key and <AvgRating> as Value
+ * Reducer1 will emit <State, MovieTitle> as Key and <Rating> as Value
  
- * Mapper2 will emit <State> as Key and <AvgRating, MovieTitle> as value.
+ * Mapper3 can compute average rating; this will emit <State> as Key and <AvgRating, MovieTitle> as value.
  * Reducer2 will emit <State> as Key and <MovieTitle> as value.
  */
 
@@ -46,74 +50,248 @@ import java.io.IOException;
 public class favoriteMovie extends Configured implements Tool {
 
     // Mapper1
-    // Outputs: Key=<State, Movie> Value=<Rating>
+    // Outputs: Key=<MovieID> Value=<State,Rating>
     public static class Mapper1 extends Mapper< LongWritable, Text, Text, Text > {
          
         @Override
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        try {    
-            // Create a parser
-            JSONParser parser = new JSONParser();
-            // Get the JSON string from input and  create a json object
-            Object obj = parser.parse(value.toString());
-            // Cast to a JSONObject
-            JSONObject jsonObj = (JSONObject) obj;
-            // Get the value of the user's state and their list of ratings
-            String state = (String) jsonObj.get("respondent.state");
-            Double[] ratings = (Double[]) jsonObj.get("ratings");
-    
-        } catch (Exception e) {
-            // handle error here
+            try {    
+                // Create a parser
+                JSONParser parser = new JSONParser();
+                // Get the JSON string from input and  create a json object
+                Object obj = parser.parse(value.toString());
+                // Cast to a JSONObject
+                JSONObject jsonObj = (JSONObject) obj;
+                // Get the value of the user's state and their list of ratings
+                String state = "CA";//(String) jsonObj.get("respondent.state");
+                Double[] ratings = (Double[]) jsonObj.get("ratings");
+                
+                // Output each rating
+                for (int i = 0; i < ratings.length; i++) {
+                    context.write(new Text(Integer.toString(i+1)), new Text(state + "," + Double.toString(ratings[i])));
+                }    
+            } 
+            catch (Exception e) {
+                // Handle error here
+                System.out.println("ERROR:" + e.toString());
+                System.exit(1);
+            }
         }
-
+    }
+    
+    // Mapper 2
+    // Outputs: Key=<MovieID>, Value=<MovieTitle>
+    public static class Mapper2 extends Mapper < LongWritable, Text, Text, Text > {
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String val = value.toString();
+            String[] split = val.split(",");
+            context.write(new Text(split[0]), new Text(split[1]));  
         }
     }
 
     // Reducer1
-    // Outputs: Key=<State, MovieTitle>, Value=<AvgRating>
-    public static class Reducer1 extends Reducer< Text, Text, Text, Text> {
+    // Matches movieID's with movie titles
+    // Outputs: Key=<State, MovieTitle>, Value=<Rating>
+    public static class Reducer1 extends Reducer< Text, Text, Text, Text > {
         @Override
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            String movieTitle = "Filler Title";
+            ArrayList<String> states = new ArrayList<String>();
+            ArrayList<Double> ratings = new ArrayList<Double>();
 
-            context.write(new Text(key.toString()), new Text(frequentList));
-
+            // Iterate over the values
+            for (Text val : values) {
+                String[] splits = val.toString().split(",");
+                
+                // We have a rating
+                if (splits.length == 2) {
+                    states.add(splits[0]);
+                    ratings.add(Double.parseDouble(splits[1]));
+                }
+                // We have the movie title
+                else {
+                   movieTitle = splits[0];
+                }
+            }
+            
+            // Output
+            for (int i = 0; i < states.size(); i++) {
+                context.write(new Text(states.get(i) + "," + movieTitle), new Text(Double.toString(ratings.get(i))));
+            }
         }
     }
+    
+    // Mapper 3
+    // Reducer1 will emit <State, MovieTitle> as Key and <Rating> as Value
+    // This just passes along values from Reducer1 to Reducer2
+    /*public static class Mapper3 extends Mapper < Text, Text, Text, Text > {
+        @Override
+        public void map(Text key, Text value, Context context) throws IOException, InterruptedException {         
+            context.write(key, value);
+        }
+    }
+    
+    
+    // Reducer2
+    // Calculates the average ratings for each movie by state
+    // Outputs: Key=<State, MovieTitle>, Value=<AvgRating>
+    // Mapper3 will emit <State, MovieTitle> as Key and <Rating> as value.
+    public static class Reducer2 extends Reducer< Text, Text, Text, Text > {
+        @Override
+        public void reduce(Text key, Iterable<Text> ratings, Context context) throws IOException, InterruptedException {
+            double totalRating = 0;
+            int numRatings = 0;            
+            
+            // Iterate over the ratings to calculate the average rating by movie and state
+            for (Text rating: ratings) {
+                totalRating += Double.parseDouble(rating.toString());
+                numRatings++;
+            }
+        
+            context.write(key, new Text(Double.toString(totalRating / numRatings)));
+        }
+    }
+    
+    // Mapper 4
+    // Input: Key=<State, MovieTitle>, Value=<AvgRating>
+    // Output: Key=<State> and Value=<MovieTitle, AvgRating>
+    public static class Mapper4 extends Mapper < Text, Text, Text, Text > {
+        @Override
+        public void map(Text key, Text value, Context context) throws IOException, InterruptedException {         
+            String[] stateAndMovie = key.toString().split(",");
+            String state = stateAndMovie[0].trim();
+            String movieTitle = stateAndMovie[1].trim();
+            
+            String movieAndRating = movieTitle + "," + value.toString();
+            
+            context.write(new Text(state), new Text(movieAndRating));
+        }
+    }
+    
+    // Needs to find the max avgRating per state and movietitle
+    // Input: Key=<State>, Value=<MovieTitle, AvgRating>
+    public static class Reducer3 extends Reducer< Text, Text, Text, Text > {
+        @Override
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            String state = key.toString();
+            Double maxAvgRating = new Double(0.0);
+            String favMovie = "";           
+            
+            for (Text val: values) {
+                String[] split = val.toString().split(",");
+                String curMovieTitle = split[0];
+                Double curRating = new Double(Double.parseDouble(split[1].toString()));
+                
+                if (curRating.compareTo(maxAvgRating) > 0) {
+                    maxAvgRating = curRating;
+                    favMovie = curMovieTitle;
+                }
+            }
+        
+            context.write(key, new Text(favMovie));
+        }
+    }*/
 
-
-  public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    int res = ToolRunner.run(conf, new hashtags(), args);
-    System.exit(res);
-  }
+    
+    // args = 
+    //   json file,
+    //   movies.csv,
+    //   output directory
     @Override
     public int run(String args[]) throws Exception {
+      // MapReduce Job #1
         Configuration conf = super.getConf();
+        
+        
         // Step 1: get a new MapReduce Job object
-        Job job = new Job(conf, "Hashtag Job");
+        Job job1 = Job.getInstance(conf);
 
         // Step 2: register the MapReduce class
-        job.setJarByClass(favoriteMovie.class);  
+        job1.setJarByClass(favoriteMovie.class);  
 
         // Step 3:  Set Input and Output files
-        FileInputFormat.addInputPath(job, new Path(args[0])); 
-        FileOutputFormat.setOutputPath(job, new Path(args[1])); 
-        job.setInputFormatClass(TextInputFormat.class); 
+        // have input file for movies.csv and for movie rating json
+        MultipleInputs.addInputPath(job1, new Path(args[0]), TextInputFormat.class, Mapper1.class);      
+//        MultipleInputs.addInputPath(job1, new Path(args[1]), TextInputFormat.class, Mapper2.class);
+        
+        FileOutputFormat.setOutputPath(job1, new Path(args[2])); 
+        //job1.setInputFormatClass(TextInputFormat.class); 
 
         // Step 4:  Register mapper and reducer
-        job.setMapperClass(FavoriteMapper.class);
-        job.setReducerClass(FavoriteReducer.class);
+        job1.setReducerClass(Reducer1.class);
 
         // Step 5: Set up output information
-        job.setOutputKeyClass(Text.class); // Specify the output class (what reduce() emits) for key
-        job.setOutputValueClass(Text.class); // Specify the output class (what reduce() emits) for value
+        job1.setOutputKeyClass(Text.class); // Specify the output class (what reduce() emits) for key
+        job1.setOutputValueClass(Text.class); // Specify the output class (what reduce() emits) for value
 
         // Step 6: Set up other job parameters at will
-        job.setJobName("Lab 8 Program 2 - Kevin Costello, Holly Haraguchi");
+        job1.setJobName("Lab 8 Program 3, Job #1 - kmcostel, hharaguc");
 
         // Step 7
-        return job.waitForCompletion(true) ? 0:1;
+//        job1.waitForCompletion(true);
+
+    /* // MapReduce Job #2
+        // Step 1: get a new MapReduce Job object
+        Job job2 = Job.getInstance();
+
+        // Step 2: register the MapReduce class
+        job2.setJarByClass(favoriteMovie.class);  
+
+        // Step 3:  Set Input and Output files
+        // Set input to be the output part-r-00000 file from job #1
+        FileInputFormat.addInputPath(job2, new Path("./test/job1Out/part-r-00000")); 
+        FileOutputFormat.setOutputPath(job2, new Path("./test/job2Out")); 
+        
+        job2.setInputFormatClass(TextInputFormat.class); 
+
+        // Step 4:  Register mapper and reducer
+        job2.setMapperClass(Mapper3.class);
+        job2.setReducerClass(Reducer2.class);
+
+        // Step 5: Set up output information
+        job2.setOutputKeyClass(Text.class); // Specify the output class (what reduce() emits) for key
+        job2.setOutputValueClass(Text.class); // Specify the output class (what reduce() emits) for value
+
+        // Step 6: Set up other job parameters at will
+        job2.setJobName("Lab 8 Program 3, Job #2 - kmcostel, hharaguc");
+
+        // Step 7
+        job2.waitForCompletion(true);
+        
+        // MapReduce Job #3
+        // Step 1: get a new MapReduce Job object
+        Job job3 = Job.getInstance();
+
+        // Step 2: register the MapReduce class
+        job3.setJarByClass(favoriteMovie.class);  
+
+        // Step 3:  Set Input and Output files
+        // Set input to be the output part-r-00000 file from job #1
+        FileInputFormat.addInputPath(job3, new Path("./test/job2Out/part-r-00000")); 
+        FileOutputFormat.setOutputPath(job3, new Path(args[2])); 
+        
+        job3.setInputFormatClass(TextInputFormat.class); 
+
+        // Step 4:  Register mapper and reducer
+        job3.setMapperClass(Mapper4.class);
+        job3.setReducerClass(Reducer3.class);
+
+        // Step 5: Set up output information
+        job3.setOutputKeyClass(Text.class); // Specify the output class (what reduce() emits) for key
+        job3.setOutputValueClass(Text.class); // Specify the output class (what reduce() emits) for value
+
+        // Step 6: Set up other job parameters at will
+        job3.setJobName("Lab 8 Program 3, Job #3 - kmcostel, hharaguc");
+*/
+        // Step 7
+        return job1.waitForCompletion(true) ? 0 : 0;
+                 
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        int res = ToolRunner.run(conf, new favoriteMovie(), args);
+        System.exit(res);
     }
 }
-
-
