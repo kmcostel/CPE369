c4c31458aa965c6a025ddb65c70ba19758cc4a6a
kmcostel <kmcostel@calpoly.edu>
2017-03-05 20:16:19 -0800

    Program 3 done

--- lab8/src/prog3/clean.sh
+++ lab8/src/prog3/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm *.class
-hdfs dfs -rm -r prog3-out
+hdfs dfs -rm -r test/
 # hdfs dfs -rm prog2/*
 # hdfs dfs -rmdir lab8_outputs/prog2
--- lab8/src/prog3/compileAndRunFave.sh
+++ lab8/src/prog3/compileAndRunFave.sh
@@ -4,5 +4,5 @@ hadoop com.sun.tools.javac.Main favoriteMovie.java
 
 jar cvf favoriteMovie.jar favoriteMovie*class
 
-hadoop jar favoriteMovie.jar favoriteMovie -libjars ../json-simple-1.1.1.jar favoriteMovie-input.json movies.csv prog3-out
+hadoop jar favoriteMovie.jar favoriteMovie -libjars ../json-simple-1.1.1.jar movies.csv favoriteMovie-input.json test/final
 
