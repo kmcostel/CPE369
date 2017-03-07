a028b5249baa9c7b47ccbc8fde7d4d9cca38a344
Holly <hharaguc@calpoly.edu>
2017-03-07 10:53:46 -0800

    Initial commit

---
+++ lab9/LargestValue.java
@@ -0,0 +1,4 @@
+// Holly Haraguchi and Kevin Costello
+// CSC 369, Winter 2017
+// Lab 11, Problem 2
+// Computes the number of hands with the highest value in each file
---
+++ lab9/PowerDays.java
@@ -0,0 +1,4 @@
+// Holly Haraguchi and Kevin Costello
+// CSC 369, Winter 2017
+// Lab 11, Problem 7
+// For each year, finds the day with the highest power consumption
